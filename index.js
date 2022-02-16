34356a6defadab48ee09e43feae7e58d7eafbc99
akshaysrb30 <98171999+akshaysrb30@users.noreply.github.com>
2022-02-16 13:10:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 initializeSDK();
 
-document.getElementById("test").onload = function initializeSDK() {
+function initializeSDK() {
     window.XmBindId.initialize({
         clientId: '9e96904e.3255a2cc.tid_5a1cbf13.bindid.io',
         apiCompat: window.XmBindId.XmBindIdApiCompatibilityLevel.UseLatest
