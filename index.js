f480ac27c03ae2b004c4a48d6f27ace26121aa33
akshaysrb30 <98171999+akshaysrb30@users.noreply.github.com>
2022-02-16 13:04:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // document.querySelectorAll('h1')[0].innerHTML="john Doe";
 
-object.onload = function initializeSDK() {
+document.getElementById("test").onload = function initializeSDK() {
     window.XmBindId.initialize({
         clientId: '9e96904e.3255a2cc.tid_5a1cbf13.bindid.io',
         apiCompat: window.XmBindId.XmBindIdApiCompatibilityLevel.UseLatest
@@ -12,16 +12,4 @@ object.onload = function initializeSDK() {
 initializeSDK();
 
 
-object.onload = function invokeBindId() {
-    window.XmBindId.authenticate({
-        redirectUri: 'https://akshaysrb30.github.io/cv/',
-        scope: [window.XmBindId.XmBindIdScopeType.Phone, window.XmBindId.XmBindIdScopeType.Email],
-        verifications: [window.XmBindId.XmRequiredVerifications.Phone, window.XmBindId.XmRequiredVerifications.Email],
-    }).then(res => {
-        onSuccess(res);
-    }, err => {
-        onFailure(err);
-    })
-}
 
-authenticate();
