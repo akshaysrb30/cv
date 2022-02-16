ca5a8dc6803fb5b2d6c18aebc5505bfae3d98858
akshaysrb30 <98171999+akshaysrb30@users.noreply.github.com>
2022-02-16 10:29:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 document.querySelectorAll('h1')[0].innerHTML="john Doe";
 
+function initializeSDK() {
+    window.XmBindId.initialize({
+        clientId: '9e96904e.3255a2cc.tid_5a1cbf13.bindid.io',
+        apiCompat: window.XmBindId.XmBindIdApiCompatibilityLevel.UseLatest
+    }).then(res => {
+        alert('SDK Initialized');
+    });
+}
+
+function invokeBindId() {
+    window.XmBindId.authenticate({
+        redirectUri: 'https://akshaysrb30.github.io/cv/'
+    }).then(res => {
+        onSuccess(res);
+    }, err => {
+        onFailure(err);
+    })
+}
