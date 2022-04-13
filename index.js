57535ae5aa1022c1fd96e8a9d40b0759b782ce0b
akshaysrb30 <98171999+akshaysrb30@users.noreply.github.com>
2022-04-13 11:44:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 
 
+function initializeSDK() {
+    window.XmBindId.initialize({
+        clientId: 'f634b341.e782e78f.tid_d166180a.bindid.io',
+    }).then(res => {
+        alert('SDK Initialized');
+    });
+}
 
+initializeSDK();
 
 
 
